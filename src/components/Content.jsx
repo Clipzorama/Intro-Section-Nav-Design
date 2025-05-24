6acd7349a4ff62a150c0069da21ea88a4e3d61f2
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-05-24 18:24:44 -0400

    add content infrastructure

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,6 +7,12 @@ function Content() {
     return (
         <div className="content-container">
             <div className="left-content">
+                <h1>Make remote work</h1>
+                <p>Get your team in sync, no matter your location. Streamline processes, 
+                create team rituals, and watch productivity soar.</p>
+                <div className="company-ads">
+                    
+                </div>
 
             </div>
         </div>
