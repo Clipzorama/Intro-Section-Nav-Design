b361078094014a5d3cd507aa2e2bb73618459d2a
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-05-24 18:26:37 -0400

    added content company ads

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,11 @@
 import '../styles/Content.css'
 
 import MainLogo from '../assets/images/image-hero-desktop.png'
+import Databiz from '../assets/images/client-databiz.svg'
+import Audiophile from '../assets/images/client-audiophile.svg'
+import Meet from '../assets/images/client-meet.svg'
+import Maker from '../assets/images/client-maker.svg'
+
 
 
 function Content() {
@@ -11,7 +16,7 @@ function Content() {
                 <p>Get your team in sync, no matter your location. Streamline processes, 
                 create team rituals, and watch productivity soar.</p>
                 <div className="company-ads">
-                    
+
                 </div>
 
             </div>
